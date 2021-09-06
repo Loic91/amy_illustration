2846e874c028cbf330c3256116d288fae22766b2
Loic91 <62215559+Loic91@users.noreply.github.com>
2021-09-06 12:41:59 +0200

    Add files via upload

--- locoScrollstrigger.js
+++ locoScrollstrigger.js
@@ -224,6 +224,317 @@ ScrollTrigger.create({
     // markers: true
 })
 // **********************************
+ScrollTrigger.create({
+    trigger: '.view',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('.view', 2, {
+        x: "60",
+        ease: Power3.easeInOut,
+        opacity: "0",
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '.col',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('.col', 2, {
+        x: "-60",
+        ease: Power3.easeInOut,
+        opacity: "0",
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '.svg_line_2',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('.svg_line_2', 2, {
+        // x: "60",
+        ease: Power3.easeInOut,
+        opacity: "0",
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '.lec',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('.lec', 2, {
+        x: "60",
+        ease: Power3.easeInOut,
+        opacity: "0",
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '.tio',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('.tio', 2, {
+        x: "-60",
+        ease: Power3.easeInOut,
+        opacity: "0",
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '.svg_line_3',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('.svg_line_3', 2, {
+        // x: "60",
+        ease: Power3.easeInOut,
+        opacity: "0",
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '.ns',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('.ns', 2, {
+        x: "-60",
+        ease: Power3.easeInOut,
+        opacity: "0",
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '.arrow',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('.arrow', 2, {
+        x: "-60",
+        ease: Power3.easeInOut,
+        opacity: "0",
+    }),
+    // markers: true
+})
+// **********************************
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_1',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_1', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_2',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_2', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_3',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_3', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_4',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_4', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_5',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_5', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_6',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_6', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+// ***********************
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_1_mobile',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_1_mobile', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_2_mobile',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_2_mobile', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_3_mobile',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_3_mobile', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_4_mobile',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_4_mobile', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_5_mobile',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_5_mobile', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_6_mobile',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_6_mobile', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_7_mobile',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_7_mobile', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_8_mobile',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_8_mobile', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_9_mobile',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_9_mobile', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_10_mobile',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_10_mobile', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+ScrollTrigger.create({
+    trigger: '#yellow_phraz_11_mobile',
+    scroller: '.smooth-scroll',
+
+    start: "top bottom",
+    end: "top top",
+    animation: gsap.from('#yellow_phraz_11_mobile', 2, {
+        y: '100%',
+        delay: 0.9
+    }),
+    // markers: true
+})
+
 
 
 
